c3b3b356e2377e278c31d0b72c0fc76292531efb
Samuel Christian <samuelchristian769@gmail.com>
2025-02-04 21:57:55 +0100

    updatte ro index.php

--- home.php
+++ home.php
@@ -2,18 +2,18 @@
 echo $_SERVER["REQUEST_METHOD"]."<br/>";
 ?>
 <form action='' method='post'> 
-Name:<input type='text' name='job'><br>
-Age:<input type='number' name='age'><br>
- <input type='submit' value='submit'><br>
+Name:<input type='text' name='john'><br/>
+Age:<input type='number' name='age'><br/>
+ <input type='submit' value='submit'><br/>
 </form>
 <?php
  if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = $_POST['job'];
+  $name = $_POST['john'];
   $age = $_POST['age'];
-  echo "Your name is: ". $name. "<br>";
-  echo "Your age is: ". $age. "<br>";
-// echo $_POST['job'] . "<br>";
-// echo $_POST['age'] . "<br>";
+  echo "Your name is: ". $name. "<br/>";
+  echo "Your age is: ". $age. "<br/>";
+// echo $_POST['job'] . "<br/>";
+// echo $_POST['age'] . "<br/>";
  }
  
 ?>
